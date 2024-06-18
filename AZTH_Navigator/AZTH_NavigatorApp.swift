93cf7edc670935fe9972b895ca4288783892f2e6
DRSaz <schmidliaz@gmail.com>
2024-06-18 09:30:08 -0700

    Initial Commit

---
+++ AZTH_Navigator/AZTH_NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AZTH_NavigatorApp.swift
+//  AZTH_Navigator
+//
+//  Created by Infinite Novice on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct AZTH_NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
